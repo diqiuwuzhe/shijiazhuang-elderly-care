b8d52fcba0ccb3316d48d30e589de1927a9ddcd2
diqiuwuzhe <61043807+diqiuwuzhe@users.noreply.github.com>
2025-04-11 15:15:29 +0800

    Add files via upload

--- 养老助手网页版/src/router/index.js
+++ 养老助手网页版/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/elderly-care-web/'),
   routes: [
     {
       path: '/',
--- 养老助手网页版/vite.config.js
+++ 养老助手网页版/vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/elderly-care-web/',
   server: {
     port: 3000,
     host: true
